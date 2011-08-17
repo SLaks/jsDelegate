dc94982eb6fb35c57f2ffaac3ef24e42ff238adb
SLaks <Schabse@GMail.com>
2011-08-17 10:55:42 -0400

    Basic delegate creation & combination

---
+++ jsDelegate.Tests.js
@@ -0,0 +1,41 @@
+﻿/// <reference path="jsDelegate.js" />
+/// <reference path="QUnit/qunit.js" />
+
+function argReturner(arg) { return arg; }
+function thisReturner() { return this; }
+
+var obj = { name: "Sample this" };
+
+test("Closed delegates", function () {
+	var d = Delegate.createClosed(obj, thisReturner);
+	equal(d(), obj);
+	equal(d(-20), obj);
+
+	d = Delegate.createClosed([1, 2, 3], "join");
+	equal(d.method, Array.prototype.join, "Create closed delegate by method name");
+	equal(d(','), "1,2,3");
+
+});
+test("Open delegates", function () {
+	var d = Delegate.createOpen(thisReturner);
+	equal(d(-20), window, "Open delegate passes no this");
+
+	d = Delegate.createOpen(argReturner);
+	equal(d(8), 8);
+	equal(d(8, -20), 8);
+});
+
+test("OpenThis delegates", function () {
+	var d = Delegate.createOpenThis(thisReturner);
+	equal(d(obj), obj);
+	equal(d(obj, -20), obj);
+
+	d = Delegate.createOpenThis(argReturner);
+	ok(typeof d(-20) === "undefined", "OpenThis delegate should pass this before function arguments");
+	equal(d(-20, 16), 16, "OpenThis delegate passes second argument as first parameter");
+});
+
+test("Combined delegates", function () {
+	var arr = [];
+	var pusher = Delegate.createClosed(arr, "push");
+});
---
+++ jsDelegate.js
@@ -0,0 +1,74 @@
+﻿var Delegate = (function () {
+	"use strict";
+	function bind(func, s) {
+		return function () { return func.apply(s, arguments); };
+	}
+	function isDelegate(func) {
+		return typeof func === "function" && typeof (func.push) === "function" && typeof (func.method) === "function";
+	}
+	function ensureDelegate(func) {
+		if (typeof func !== "function")
+			throw new Error("argument is not a function or delegate");
+		return isDelegate(func) ? func : createDelegate(func, func);
+	}
+
+
+	function createDelegate(invoker, method, previous) {
+		var d = function delegateFunc() {
+			if (typeof previous === "function")
+				previous.apply(this, arguments);
+			return invoker.apply(this, arguments);
+		};
+
+		d.method = method;
+		if (typeof previous === "function")
+			d.previous = previous;
+
+		d.push = function (prevDelegate) {
+			if (!prevDelegate)
+				return d;
+			prevDelegate = ensureDelegate(prevDelegate);
+			var newPrevious;
+
+			//If this delegate is itself the head of a 
+			//chain, clone the whole chain. Otherwise,
+			//we can add this method to the new chain 
+			//directly.
+			if (typeof previous === "function")
+				newPrevious = previous.push(prevDelegate);
+			else
+				newPrevious = prevDelegate;
+			return createDelegate(invoker, method, newPrevious);
+		};
+
+		return d;
+	}
+
+	return {
+		isDelegate: isDelegate,
+		createOpen: function (method) {
+			return createDelegate(method, method);
+		},
+		createOpenThis: function (method) {
+			return createDelegate(bind(method.call, method), method);
+		},
+		createClosed: function (thisObj, method) {
+			if (typeof method === "string")
+				method = thisObj[method];
+			return createDelegate(bind(method, thisObj), method);
+		},
+		combine: function () {
+			var retVal = null;
+			for (var i; i < arguments.length; i++) {
+				if (!arguments[i])
+					continue;
+
+				if (!retVal)
+					retVal = ensureDelegate(arguments[i]);
+				else
+					retVal = retVal.push(arguments[i]);
+			}
+			return retVal;
+		}
+	};
+})();
