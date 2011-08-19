20a05d1a5284d4f398f77694063de532fefd342c
SLaks <Schabse@GMail.com>
2011-08-19 16:19:42 -0400

    Cleanup, tree multicast

--- jsDelegate.js
+++ jsDelegate.js
@@ -3,8 +3,38 @@
 	function bind(func, s) {
 		return function () { return func.apply(s, arguments); };
 	}
+
+	function extend(target, source) {
+		for (var cProp in source)
+			if (source.hasOwnProperty(cProp))
+				target[cProp] = source[cProp];
+
+		//Work around IE bug (toString & valueOf aren't enumerated)
+		if (source.hasOwnProperty("toString"))
+			target.toString = source.toString;
+		if (source.hasOwnProperty("valueOf"))
+			target.valueOf = source.valueOf;
+
+		return target;
+	}
+
+	var delegateMethods = {
+		curry: function () {
+			var preArgs = Array.prototype.slice.call(arguments);
+			var delegate = this;
+			return createDelegate(function () {
+				return delegate.apply(this, preArgs.concat(Array.prototype.slice.call(arguments)));
+			}, delegate.method);
+		},
+		each: function (callback) {
+			for (var i = 0; i < this.previous.length; i++) {
+				this.previous[i].each(callback);
+			}
+			return callback(this, this.invoker);
+		}
+	};
 	function isDelegate(func) {
-		return typeof func === "function" && func.curry === curry && typeof (func.push) === "function" && typeof (func.method) === "function";
+		return typeof func === "function" && func.curry === delegateMethods.curry && typeof (func.push) === "function" && typeof (func.method) === "function";
 	}
 	function ensureDelegate(func) {
 		if (typeof func !== "function")
@@ -12,43 +42,32 @@
 		return isDelegate(func) ? func : createDelegate(func, func);
 	}
 
-	function curry() {
-		var preArgs = Array.prototype.slice.call(arguments);
-		var delegate = this;
-		return createDelegate(function () {
-			return delegate.apply(this, preArgs.concat(Array.prototype.slice.call(arguments)));
-		}, delegate.method);
-	}
-
 	function createDelegate(invoker, method, previous) {
+		if (typeof invoker !== "function")
+			throw new Error("Function expected");
+		if (typeof method !== "function")
+			throw new Error("Function expected");
+		if (previous && !(previous instanceof Array))
+			throw new Error("Previous is not an array");
+
 		var d = function delegateFunc() {
-			if (typeof previous === "function")
-				previous.apply(this, arguments);
-			return invoker.apply(this, arguments);
+			var args = arguments;
+			var target = this;
+
+			return d.each(function (delegate, invoker) {
+				return invoker.apply(target, args);
+			});
 		};
-		
 
 		d.method = method;
-		d.curry = curry;
-		if (typeof previous === "function")
-			d.previous = previous;
-		d.prepend = function (prevDelegate) {
-			if (!prevDelegate)
-				return d;
-			prevDelegate = ensureDelegate(prevDelegate);
-			var newPrevious;
+		d.invoker = invoker;
+		extend(d, delegateMethods);
 
-			//If this delegate is itself the head of a 
-			//chain, clone the whole chain. Otherwise,
-			//we can add this method to the new chain 
-			//directly.
-			if (typeof previous === "function")
-				newPrevious = previous.push(prevDelegate);
-			else
-				newPrevious = prevDelegate;
+		d.copy = function (newPrevious) {
 			return createDelegate(invoker, method, newPrevious);
 		};
 
+		d.previous = previous || [];
 		return d;
 	}
 
@@ -71,10 +90,15 @@
 				if (!arguments[i])
 					continue;
 
+				var next = arguments[i];
 				if (!retVal)
-					retVal = ensureDelegate(arguments[i]);
-				else
-					retVal = ensureDelegate(arguments[i]).prepend(retVal);
+					retVal = ensureDelegate(next);
+				else if (!isDelegate(next))
+					retVal = createDelegate(next, next, [retVal]);
+				else {
+					var newPrevious = [retVal].concat(next.previous);
+					retVal = next.copy(newPrevious);
+				}
 			}
 			return retVal;
 		}
